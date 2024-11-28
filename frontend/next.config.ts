f311509166477dbfa62886f55794745291dd698c
kuramasa <kuramasa.green@gmail.com>
2024-11-28 21:45:33 +0900

    設定変更

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  distDir: './public',
+  distDir: './',
 }
 
 export default nextConfig
